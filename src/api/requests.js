3689eee055c83a68cb31bb0644fef4d6b71d534e
cristopherva <ing.valentin99@hotmail.com>
2022-03-16 14:14:23 -0400

    apikey update

--- src/api/requests.js
+++ src/api/requests.js
@@ -1,4 +1,5 @@
-const API_KEY = process.env.API_KEY // USE YOUT API KEY TMDB
+
+const API_KEY = '386c8c93b20450ffe7dfb37d3e0eec85' // USE YOUT API KEY TMDB
 
 const requests = {
     fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
